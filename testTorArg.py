18deb31ef73408668d01bfc1bdf4dba0156db5ac
yoy <36720031+yoyboy@users.noreply.github.com>
2019-04-05 12:10:26 +0800

    Update testTorArg.py

--- testTorArg.py
+++ testTorArg.py
@@ -31,9 +31,50 @@ get_argument(name, default = _ARG_DEFAULT, strip = True)
 get_arguments(name, strip = True)
 从请求体和查询字符串中返回指定参数name的值, 返回的是list列表(即使对应name参数只有一个值). 若没有name参数, 则返回空列表[]
 
+使用get_argument / get_arguments, 如果get / post都有同样的参数名称, 会按顺序先取出get的参数再取出post的参数
+因为get是放在请求头里面的, 按照http解析顺序会先取get里面的参数. 而post是放在请求体里面的, 所以是后取
+所以上述条件中, get_argument获取的参数是body里面最后一个参数
+
 对于json和xml无法通过上述方法获取
 """
 
 """
 关于default的默认值_ARG_DEFAULT
+是一个全局类, 专门用来判断是否设置了与_ARG_DEFAULT不一样的对象, 类里面不做任何处理
+class _ArgDefaultMarker:
+    pass
 """
+
+import tornado.web
+import tornado.ioloop
+import tornado.httpserver
+import tornado.options
+from tornado.options import options, define
+from tornado.web import RequestHandler, MissingArgumentError
+
+define("port", default = 8000, type = int, help = "run server on the given port.")
+
+class InderHandler(RequestHandler):
+    def get(self):
+        # query_arg = self.get_query_argument("query", default = 1)
+        # query_args = self.get_query_arguments("query")
+        query_arg = self.get_argument("query", default = 10)
+        query_args = self.get_arguments("query")
+        rep = "query_arg: {}</br>query_args: {}</br>".format(query_arg, query_args)
+        self.write(rep)
+    def post(self): # 使用postman Body(x-www-form-urlencoded)测试
+        # body_arg = self.get_body_argument("body", default = 10)
+        # body_args = self.get_body_arguments("body")
+        body_arg = self.get_argument("body", default = 13)
+        body_args = self.get_arguments("body")
+        rep = "body_arg: {}</br>body_args: {}</br>".format(body_arg, body_args)
+        self.write(rep)
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application([
+        (r"/", InderHandler)
+    ], debug = True)
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
