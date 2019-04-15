706a613658d5b005d3f3c6bf8f5ea4f708c76cbd
yoyboy <534411590@qq.com>
2019-04-15 15:32:38 +0800

    create File

---
+++ Tornado_Project/config.py
@@ -0,0 +1,31 @@
+import os
+
+log_level = "debug"
+log_file = os.path.join(os.path.dirname(__file__), "logs/log.txt")
+
+settings = dict(
+    debug = True,
+    static_path = os.path.join(os.path.dirname(__file__), "static"),
+    template_path = os.path.join(os.path.dirname(__file__), "template"),
+    cookie_secret = "",
+    xsrf_cookies = True,
+    login_url = "/login", 
+)
+
+mysql_options = dict(
+    max_connections = 20, #max open connections
+    idle_seconds = 7200, #conntion idle timeout time, 0 is not timeout
+    wait_connection_timeout = 3, #wait connection timeout
+    host = "",
+    user = "",
+    passwd = "",
+    db = "",
+    charset = ""
+)
+
+redis_options = dict(
+    host = "", 
+    port = 0, 
+    db = 0, 
+    password = ""
+) 
