9ca0ac9b0c2b137153a6ccb28caef5dedf20046e
yoyboy <534411590@qq.com>
2019-04-15 15:13:20 +0800

    update some files

--- Tornado_Project/urls.py
+++ Tornado_Project/urls.py
@@ -7,7 +7,7 @@ from tornado.web import StaticFileHandler
 current_path = os.path.dirname(__file__)
 
 handlers = [
-    (r"/", passPort.IndexHandler),
+    # (r"/", passPort.IndexHandler),
     (r"/api/imagecode", verifyCode.ImageCodeHandler),
     (r"^/(.*)$", StaticFileHandler, {"path": os.path.join(current_path, "html"), "default_filename": "index.html"}),
 ]
