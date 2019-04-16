52e24bee65490d7dcbdea0f5a22b42d1d61588bc
yoyboy <534411590@qq.com>
2019-04-16 13:35:51 +0800

    async 数据库查询

--- Tornado_Project/handlers/passPort.py
+++ Tornado_Project/handlers/passPort.py
@@ -3,6 +3,8 @@ import json
 import string
 import random
 import re
+import asyncio
+import tornado.ioloop
 
 from tornado.web import RequestHandler
 from utils.response_code import RET
@@ -18,7 +20,7 @@ class RegisterHandler(RequestHandler):
             if not self.dataBody:
                 self.dataBody = dict()
         print(self.dataBody)
-    def post(self):
+    async def post(self):
         mobile = self.dataBody.get("mobile")
         phoneCode = self.dataBody.get("phonecode")
         passwd = self.dataBody.get("password")
@@ -36,7 +38,7 @@ class RegisterHandler(RequestHandler):
             check_phonecode = self.application.redis.get("sms_code_%s" % mobile).decode("utf8")
         except Exception as e:
             logging.error(e)
-            return self.write(dict(errno = RET.DBERR, errmsg = "查询出错"))
+            return self.write(dict(errno = RET.DBERR, errmsg = "redis查询出错"))
 
         # 手机验证码过期
         if not check_phonecode:
@@ -55,6 +57,14 @@ class RegisterHandler(RequestHandler):
         判断手机有没在mysql里面
         没有才继续往下执行, 否则return
         """
+        task = []
+        # task = asyncio.ensure_future(self.get_user_phone(mobile))
+        task.append(asyncio.ensure_future(self.get_user_phone(mobile)))
+        for done in asyncio.as_completed(task):
+            data = await done
+            # 有数据的话, 代表该手机已经被注册
+            if data:
+                return self.write(dict(errno = RET.PARAMERR, errmsg = "手机被注册了"))
 
         # 不能有空格
         if len(passwd.strip()) != len(passwd):
@@ -65,3 +75,17 @@ class RegisterHandler(RequestHandler):
             return self.write(dict(errno = RET.PARAMERR, errmsg = "两次密码不一样"))
 
         return self.write(dict(errno=RET.OK, errmsg = "注册成功"))
+
+    async def get_user_phone(self, user_phone):
+        datas = None
+        SQL = "select up_mobile from ih_user_profile where up_mobile = %s"
+        async with await self.application.db.Connection() as conn:
+            try:
+                async with conn.cursor() as cursor:
+                    await cursor.execute(SQL, user_phone)
+                    datas = cursor.fetchone()
+                    print("datas: ", datas)
+            except Exception as e:
+                logging.error(e)
+                return self.write(dict(errno = RET.DBERR, errmsg = "mysql查询出错"))
+        return datas
