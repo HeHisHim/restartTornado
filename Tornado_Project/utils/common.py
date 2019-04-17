dd62b2ec0f050b80db0b2edb14bdcf6da79fbd48
yoyboy <534411590@qq.com>
2019-04-17 17:08:30 +0800

    create get_current_user

--- Tornado_Project/utils/common.py
+++ Tornado_Project/utils/common.py
@@ -1,5 +1,7 @@
 from utils.response_code import RET
 import functools
+import logging
+import json
 
 def require_logined(fun):
     @functools.wraps(fun)
@@ -10,3 +12,18 @@ def require_logined(fun):
         else:
             fun(request_handler, *args, **kwargs)
     return wrapper
+
+def get_current_user(handler):
+    if handler.get_secure_cookie("session_id"):
+        handler.session_id = handler.get_secure_cookie("session_id").decode("utf8")
+        try:
+            handler.user_data = handler.application.redis.get("sess_%s" % handler.session_id)
+        except Exception as e:
+            logging.error(e)
+            return False
+        if handler.user_data:
+            handler.user_data = json.loads(handler.user_data)
+            return True
+        else:
+            return False
+    return False
