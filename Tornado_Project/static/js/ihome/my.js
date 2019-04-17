1c89d4cdc9aa091c1778704a63b6d3935432e494
yoyboy <534411590@qq.com>
2019-04-17 17:07:43 +0800

    miss

--- Tornado_Project/static/js/ihome/my.js
+++ Tornado_Project/static/js/ihome/my.js
@@ -1,6 +1,6 @@
 function logout() {
     $.get("/api/logout", function(data){
-        if (0 == data.errcode) {
+        if ("0" == data.errno) {
             location.href = "/";
         }
     })
