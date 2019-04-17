e0df18db0d3906f254c8cd9ee9efbec909c575a7
yoyboy <534411590@qq.com>
2019-04-17 12:32:49 +0800

    Update application_init.py
    同步操作数据库

--- Tornado_Project/handlers/application_init.py
+++ Tornado_Project/handlers/application_init.py
@@ -3,9 +3,11 @@ import tornado.web
 import urls
 import config
 import redis
+import pymysql
 
 class Application(tornado.web.Application):
     def __init__(self):
         tornado.web.Application.__init__(self, handlers = urls.handlers, **config.settings)
         self.db = tormysql.ConnectionPool(**config.mysql_options)
         self.redis = redis.Redis(**config.redis_options)
+        self.puredb = pymysql.connect(**config.pure_mysql_options)
