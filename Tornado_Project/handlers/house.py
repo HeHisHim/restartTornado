315a8ce05b337fca8b61c7cc32441356fded96fa
yoyboy <534411590@qq.com>
2019-04-18 15:39:37 +0800

    create MyHouseHandler

--- Tornado_Project/handlers/house.py
+++ Tornado_Project/handlers/house.py
@@ -19,7 +19,6 @@ class AreaIngoHandler(RequestHandler):
         results = self.get_area_info_inCache()
         if results:
             results = results.decode("utf8")
-            print(results, json.loads(results))
             return self.write(dict(errno = RET.OK, errmsg = "OK", data = json.loads(results)))
 
 
@@ -67,5 +66,42 @@ class AreaIngoHandler(RequestHandler):
             except Exception as e:
                 logging.error(e)
                 return self.write(dict(errno = RET.DBERR, errmsg = "mysql查询出错"))
-        print("datas: ", datas, tmp)
+        return datas
+
+class MyHouseHandler(RequestHandler):
+    def get_current_user(self):
+        self.user_data = dict()
+        return utils.common.get_current_user(self)
+
+    @utils.common.require_logined
+    def get(self):
+        uid = self.user_data.get("uid")
+        results = self.get_house_info(uid)
+        houses = []
+        if results and results[0]:
+            for result in results:
+                house = {"area_id": result[0], "name": result[1]}
+                house = {
+                    "house_id": result[0], 
+                    "title": result[1], 
+                    "price": result[2],
+                    "ctime": result[3].strftime("%Y-%m-%d"),
+                    "area_name": result[4],
+                    "img_url": None
+                }
+                houses.append(house)
+        return self.write(dict(errno = RET.OK, errmsg = "OK", houses = houses))
+
+
+
+    def get_house_info(self, uid):
+        datas = None
+        SQL = "select x.hi_house_id, x.hi_title, x.hi_price, x.hi_utime, y.ai_name, x.hi_index_image_url from ih_house_info as x left join ih_area_info as y on x.hi_area_id = y.ai_area_id where x.hi_user_id = %s;"
+        with self.application.puredb.cursor() as cursor:
+            try:
+                cursor.execute(SQL, uid)
+                datas = cursor.fetchall()
+            except Exception as e:
+                logging.error(e)
+                return self.write(dict(errno = RET.DBERR, errmsg = "mysql出错"))
         return datas
